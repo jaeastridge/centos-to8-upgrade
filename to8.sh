1c11f5483e661322abb0692a624a7f1798141032
jeastri1 <jeastri1@TMC02XL1HZJGH6>
2019-11-14 21:44:31 -0700

    fix for EPEL part 2

--- to8.sh
+++ to8.sh
@@ -159,7 +159,7 @@ cat >$STAGING_DIR/etc/yum.repos.d/epel.repo <<EOF
 [epel]
 name=Extra Packages for Enterprise Linux 8
 #baseurl=https://download.fedoraproject.org/pub/epel/\$releasever/Everything/\$basearch
-metalink=https://mirrors.fedoraproject.org/metalink?repo=epel-\$releasever&arch=\$basearch&infra=\$infra&content=\$contentdir
+metalink=https://mirrors.fedoraproject.org/metalink?repo=epel-8&arch=x86_64
 enabled=1
 gpgcheck=1
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-8
